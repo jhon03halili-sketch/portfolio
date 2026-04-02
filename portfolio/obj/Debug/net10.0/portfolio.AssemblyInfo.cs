cc49c773c2f1f728b119e3a3f4412dbdf70ca15c
Jhon Carlo <jhon03halili@gmail.com>
2026-04-02 19:31:06 +0800

    uppercase titles

--- portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
+++ portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a6195179952d2e2e1733aa75c994f39dcbaac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e83dd7695dda66eb2b5bbeee71585d46e35b6d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
