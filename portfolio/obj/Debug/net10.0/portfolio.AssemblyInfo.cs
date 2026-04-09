2d6bb988c57a23f046250d82074975ce8e0adfe7
Jhon Carlo <jhon03halili@gmail.com>
2026-04-09 11:53:00 +0800

    i think i forgot something to save yesterday

--- portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
+++ portfolio/obj/Debug/net10.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fbaa5c12ed70416f9550a06ae2a645282e7080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26aa9016c94103a15a4a79b91a358da4de27303")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
